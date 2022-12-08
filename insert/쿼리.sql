e7dbbbd0edc4024f46dfac4de883f554ed6cbc12
karatejin <junsyang@naver.com>
2022-12-08 13:01:09 +0900

    20221208 sql

---
+++ insert/쿼리.sql
@@ -0,0 +1,20 @@
+
+/* 
+Create
+Read
+Update
+Delete
+INSERT(추가) =C
+SELECT(조회) =R
+UPDATE(수정) =U
+DELETE(삭제) =D
+순서 맞춰라
+*/
+
+INSERT INTO product_mst
+/*	(id, product_name, product_price) 생락가능 */
+VALUES
+	(0, '스타벅스 블랙 머그컵', 15000),
+	(0, '스타벅스 화이트 머그컵', 20000),
+	(0, '스타벅스 블루 머그컵', 25000); order_mstproduct_mstuser_mst
+
