b7f8d2c1f28bf958342051b2e1eb3372e6ec1746
karatejin <junsyang@naver.com>
2022-12-09 12:55:56 +0900

    20221209

---
+++ study_notice #3.sql
@@ -0,0 +1,11 @@
+SELECT
+	nm.title,
+	nm.content,
+	wm.name,
+	ni.img_name
+FROM
+	study_notice_mst nm
+	LEFT OUTER JOIN study_writer_mst wm ON(wm.id = nm.writer_id)
+	LEFT OUTER JOIN study_notice_
+	img ni ON(ni.notice_id = nm.id);
+	
---
+++ user.sql
@@ -0,0 +1,14 @@
+INSERT INTO
+	user_mst
+	
+VALUES
+	(0, 'aaa', '1234', '김준일', 'aaa@gmail.com',NOW(),NOW()),
+	(0, 'bbb', '1234', '김준일', 'aaa@gmail.com',NOW(),NOW()),
+	(0, 'ccc', '1234', '김준일', 'aaa@gmail.com',NOW(),NOW()),
+	(0, 'ddd', '1234', '김준일', 'aaa@gmail.com',NOW(),NOW());
+
+DELETE
+from
+	user_mst
+WHERE
+	id = 1;
