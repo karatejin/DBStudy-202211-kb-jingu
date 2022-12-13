cd3024ecbc575fd8201d5a1a189e2a46894fb3ce
karatejin <junsyang@naver.com>
2022-12-14 08:05:27 +0900

    20221213 어제 쿼리 빠뜨린것

--- study_notice #3.sql
+++ study_notice #3.sql
@@ -9,3 +9,27 @@ FROM
 	LEFT OUTER JOIN study_notice_
 	img ni ON(ni.notice_id = nm.id);
 	
+SELECT
+	*
+FROM
+	user_mst um /* um 이라고 부르겠다.*/
+	
+	LEFT OUTER JOIN user_dtl ud ON (user_mst.id = user_dtl.id); /* ud 라고 부르겠다.*/
+	
+SELECT
+	  *
+FROM
+	order_mst om
+	LEFT OUTER JOIN order_dtl od ON (od.order_id = om.id) /*od 가 이것인것 이라는 조건?*/
+	LEFT OUTER JOIN user_mst um ON(um.id = om.user_id);
+	
+SELECT 
+	*
+
+FROM
+	user_mst um
+	LEFT OUTER JOIN order_mst om ON(om.user_id = um.id)
+	LEFT OUTER JOIN order_dtl od ON(od.order_id = om.id);
+
+
+
